30da2214cf37818de358e098a570e9a0d7b4ffb6
MarcosRanauro <marcosranauro@hotmail.com>
2024-02-02 22:08:29 -0300

    Passei as instancias dos novos orixas para o components OrixasList, e inclui Yemanja

--- candomble-e-orixas/src/pages/Orixas.jsx
+++ candomble-e-orixas/src/pages/Orixas.jsx
@@ -1,54 +1,13 @@
 import React from 'react'
-import OrixasClass from '../components/OrixasClass';
 import Header from '../components/Header';
-import OxaguianIMG from '../img/Oxaguian.jpg';
-import OxalaIMG from '../img/Oxala.jpg';
-import Nana from '../img/Nana.jpg';
+import OrixasList from '../components/OrixasList';
 import '../styles/Orixas.css';
-
-const orixas = [
-  new OrixasClass(
-    "Oxala",
-    "Orixá da paz, da harmonia e da criação.",
-    "Cajado",
-    OxalaIMG,
-    "Oxalá é o orixá da paz, da harmonia e da criação. Ele é o pai de todos os orixás, e é considerado o criador do mundo. Ele é o dono do cajado, e é o protetor dos doentes e dos idosos. Oxalá é o orixá que traz a paz e a tranquilidade, e é o responsável por manter a harmonia entre os seres humanos."
-  ),
-  new OrixasClass(
-    "Oxaguian",
-    "Orixá da juventude, da renovação e do movimento.",
-    "Espada e escudo",
-    OxaguianIMG,
-    "Oxaguian é o orixá da juventude, da renovação e do movimento. Ele é o filho de Iemanjá e Oxalá, e é considerado um orixá guerreiro. Ele é o dono da espada e do escudo, e é o protetor dos jovens e dos soldados. Oxaguian é o orixá que abre os caminhos, e é o responsável por proteger as pessoas que estão começando uma nova fase em suas vidas."
-  ),
-  new OrixasClass(
-    "Nanã",
-    "Orixá da lama, da morte e da sabedoria.",
-    "Abebé",
-    Nana,
-    "Nanã é o orixá da lama, da morte e da sabedoria. Ela é a mãe de todos os orixás, e é considerada a mais velha e a mais sábia de todos. Ela é o dona do abebé, e é a protetora dos doentes e dos idosos. Nanã é o orixá que traz a sabedoria e a tranquilidade, e é o responsável por manter a harmonia entre os seres humanos."
-  ),
-];
 
 function Orixas() {
   return (
     <>
       <Header />
-      <h1 class="d-flex flex-column align-items-center">Orixás</h1>
-      <div class="row">
-        {orixas.map((orixa) => (
-          <div class="col-md-4">
-            <div key={orixa.name} className="card" style={{ width: '18rem' }}>
-              <h5 className="card-title">{orixa.name}</h5>
-              <img src={orixa.image} alt={orixa.name} className="card-img-top"></img>
-              <div className="card-body">
-                <p className="card-text">{orixa.getFullDescription()}</p>
-                <button className="btn btn-primary">Go somewhere</button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <OrixasList />
     </>
   )
 }
